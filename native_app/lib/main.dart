46da005613b4d58fae1e6e8a7bf49f1565345407
gai268 <10540117+gai268@users.noreply.github.com>
2022-05-28 01:32:11 +0900

    flutter_inappwebview

--- native_app/lib/main.dart
+++ native_app/lib/main.dart
@@ -1,56 +1,116 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 import 'dart:io';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-void main() {
-  runApp(const MyApp());
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (Platform.isAndroid) {
+    await AndroidInAppWebViewController.setWebContentsDebuggingEnabled(true);
+  }
+
+  runApp(const MaterialApp(home: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const WebViewExample(),
-    );
-  }
+  _MyAppState createState() => new _MyAppState();
 }
 
-class WebViewExample extends StatefulWidget {
-  const WebViewExample({Key? key}) : super(key: key);
+class _MyAppState extends State<MyApp> {
+  final GlobalKey webViewKey = GlobalKey();
 
-  @override
-  WebViewExampleState createState() => WebViewExampleState();
-}
+  InAppWebViewController? webViewController;
 
-class WebViewExampleState extends State<WebViewExample> {
+  late PullToRefreshController pullToRefreshController;
+  double progress = 0;
+
   @override
   void initState() {
     super.initState();
-    // Enable virtual display.
-    if (Platform.isAndroid) WebView.platform = AndroidWebView();
+
+    pullToRefreshController = PullToRefreshController(
+      options: PullToRefreshOptions(
+        color: Colors.blue,
+      ),
+      onRefresh: () async {
+        if (Platform.isAndroid) {
+          webViewController?.reload();
+        } else if (Platform.isIOS) {
+          webViewController?.loadUrl(
+              urlRequest: URLRequest(url: await webViewController?.getUrl()));
+        }
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return const WebView(
-      initialUrl: 'http://localhost:3000',
-      javascriptMode: JavascriptMode.unrestricted,
-    );
+    return SafeArea(
+        child: InAppWebView(
+      key: webViewKey,
+      initialUrlRequest: URLRequest(url: Uri.parse("http://localhost:3000")),
+      initialOptions: InAppWebViewGroupOptions(
+          crossPlatform: InAppWebViewOptions(
+            useShouldOverrideUrlLoading: true,
+            mediaPlaybackRequiresUserGesture: false,
+          ),
+          android: AndroidInAppWebViewOptions(
+            useHybridComposition: true,
+          ),
+          ios: IOSInAppWebViewOptions(
+            allowsInlineMediaPlayback: true,
+          )),
+      pullToRefreshController: pullToRefreshController,
+      onWebViewCreated: (controller) {
+        webViewController = controller;
+      },
+      onLoadStart: (controller, url) {},
+      androidOnPermissionRequest: (controller, origin, resources) async {
+        return PermissionRequestResponse(
+            resources: resources,
+            action: PermissionRequestResponseAction.GRANT);
+      },
+      shouldOverrideUrlLoading: (controller, navigationAction) async {
+        var url = navigationAction.request.url!;
+
+        if (!["http", "https", "file", "chrome", "data", "javascript", "about"]
+            .contains(url.scheme) || url.host != "localhost") {
+          if (await canLaunchUrl(url)) {
+            // ユーザー規定のブラウザで開きます
+            await launchUrl(url);
+            return NavigationActionPolicy.CANCEL;
+          }
+        }
+        return NavigationActionPolicy.ALLOW;
+      },
+      onLoadStop: (controller, url) async {
+        pullToRefreshController.endRefreshing();
+      },
+      onLoadError: (controller, url, code, message) {
+        pullToRefreshController.endRefreshing();
+      },
+      onProgressChanged: (controller, progress) {
+        if (progress == 100) {
+          pullToRefreshController.endRefreshing();
+        }
+        setState(() {
+          this.progress = progress / 100;
+        });
+      },
+      onUpdateVisitedHistory: (controller, url, androidIsReload) {},
+      onConsoleMessage: (controller, consoleMessage) {
+        print(consoleMessage);
+      },
+    ));
   }
 }
